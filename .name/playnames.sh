710ac58405aae9c981dec7a26529c630f9466456
spanishgum <aps10d@my.fsu.edu>
2017-11-08 00:55:48 -0500

    Updated playnames loop script to center both horizontally and vertically

--- .name/playnames.sh
+++ .name/playnames.sh
@@ -1,7 +1,16 @@
+IFS=$'\n'
 while true; do
-for f in `find art -maxdepth 1 -iname '*.art' -type f | shuf`; do
-    if [ `wc -L $f | awk '{print $1}'` -lt `tput cols` ]; then
-        clear && cat $f && sleep 1
+for f in `find favs -maxdepth 1 -iname '*.art' -type f | shuf`; do
+    fcol=`wc -L $f | awk '{print $1}'`
+    frow=`wc -l $f | awk '{print $1}'`
+    col=`tput cols`
+    row=`tput lines`
+    if [ $fcol -lt $col ] && [ $frow -lt $row ]; then
+        let xcol=($col-$fcol)/2 && let xrow=($row-$frow)/2
+        yes '' | sed ${xrow}q # vertical center first, then horizontal
+        for i in $(cat $f); do printf "%*s$i" $xcol | tr ' ' " "; echo ""; done
+        sleep 1
+        clear
     fi
 done
 done
